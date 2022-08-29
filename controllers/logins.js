d000f33d439780efedce2d68d31de1205e0535b3
iTzAymen <59091390+iTzAymen@users.noreply.github.com>
2022-08-29 14:36:06 +0100

    added checkAuth and createAcc (#2)
    * Update logins.js
    
    * Update logins.js

--- controllers/logins.js
+++ controllers/logins.js
@@ -11,6 +11,45 @@ const getAllData = async (req, res) => {
     }
 }
 
+const checkAuth = async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const login = await Login.findOne({email})
+        if(!login){
+            return res.status(200).json({success: false, msg: 'Account does not exist'})
+        }
+        if(password !== login.password){
+            return res.status(200).json({success: false, msg: 'Wrong password'})
+        }
+        res.status(200).json({success: true, data: {...login, password:''}})
+    } catch (error) {
+        res.status(500).json({success: false, msg: error})
+    }
+}
+
+const createAcc = async (req, res) => {
+    try {
+        const {email, username, password} = req.body
+        const existing_email = await Login.findOne({email})
+        const existing_username = await Login.findOne({username})
+
+        if(existing_email){
+            return res.status(200).json({success: false, msg: 'Email already exists'})
+        }
+
+        if(existing_username){
+            return res.status(200).json({success: false, msg: 'Username already taken'})
+        }
+
+        const login = await Login.CreateOne(req.body)
+        res.status(200).json({success: true, data: {...login, password:''}})
+    } catch (error) {
+        res.status(500).json({success: false, msg: error})
+    }
+}
+
 module.exports = {
-    getAllData
+    getAllData,
+    checkAuth,
+    createAcc
 }
--- routes/logins.js
+++ routes/logins.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const {getAllData} = require('../controllers/logins.js')
+const {
+    getAllData,
+    checkAuth,
+    createAcc} = require('../controllers/logins.js')
 
 router.route('/')
     .get(getAllData)
+
+router.route('/auth')
+    .get(checkAuth)
+    .post(createAcc)
 
 module.exports = router
