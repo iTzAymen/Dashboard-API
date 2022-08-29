974bc8c0b82dedd3e4545cfc536b2217055a7879
iTzAymen <59091390+iTzAymen@users.noreply.github.com>
2022-08-29 15:28:20 +0100

    Final

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const dataRouter = require('./routes/transactions')
 const loginRouter = require('./routes/logins')
-//const cors = require('cors');
+const cors = require('cors');
 require('dotenv').config()
 const connectDb = require('./db/connect')
 
 const app = express()
 
-//app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use('/api/v1/transactions', dataRouter)
