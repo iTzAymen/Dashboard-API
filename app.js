9cb8042d34d3a93eb01f6c03ba01677f2dc8d8e4
iTzAymen <59091390+iTzAymen@users.noreply.github.com>
2022-08-29 14:55:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express()
 
 //app.use(cors());
 app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 app.use('/api/v1/transactions', dataRouter)
 app.use('/api/v1/login', loginRouter)
 
