6f1d7e229870121186ddb3bde102e2c88bfdc174
iTzAymen <59091390+iTzAymen@users.noreply.github.com>
2022-08-30 13:40:51 +0000

    Added getSmallData (#4)
    * Added getSmallData
    
    * Update transactions.js
    
    * Update transactions.js

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -6,7 +6,7 @@ const getAllData = async (req, res) => {
         const d = new Date()
         const transaction = await Transaction.find({})
         const t = new Date() - d
-        console.log(`query successful after ${t} ms`)
+        console.log(`getAllData successful after ${t} ms`)
         res.status(200).json({success: true, data: transaction})
     } catch (error) {
         console.log(error)
@@ -14,6 +14,44 @@ const getAllData = async (req, res) => {
     }
 }
 
+const getSmallData = async (req, res) => {
+    console.log('getting small data')
+    try {
+        const d = new Date()
+        const transaction = await Transaction.find({})
+        
+        let maxDate = null
+        let minDate = null
+        let dataset = [...transaction]
+        dataset.forEach((value, index) => {
+            const dateString = value["DATE DE CREATION"]
+            const transactionDate = dateString ? new Date(dateString) : null
+            if (transactionDate && (!maxDate || transactionDate > maxDate)) {
+                maxDate = transactionDate
+            }
+            if (transactionDate && (!minDate || transactionDate < minDate)) {
+                minDate = transactionDate
+            }
+        })
+        const newMinDate = {...maxDate}
+        newMinDate.setMonth(newMinDate.getMonth() - 3)
+        newMinDate = newMinDate < minDate ? minDate : newMinDate
+        const smallData = transaction.filter((data) => {
+            const date = data["DATE DE CREATION"]
+            const transactionDate = dateString ? new Date(dateString) : null
+            return (transactionDate && transactionDate >= newMinDate)
+        })
+        
+        const t = new Date() - d
+        console.log(`getSmallData successful after ${t} ms`)
+        res.status(200).json({success: true, data: smallData})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success: false, msg: error})
+    }
+}
+
 module.exports = {
-    getAllData
+    getAllData,
+    getSmallData
 }
