e2402ec86c14c9b59c57773c1c331acd2a39e4c0
iTzAymen <59091390+iTzAymen@users.noreply.github.com>
2022-08-30 14:40:03 +0100

    Update transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getAllData} = require('../controllers/transactions')
+const {getAllData, getSmallData} = require('../controllers/transactions')
 
 router.route('/')
     .get(getAllData)
+
+router.route('/small')
+    .get(getSmallData)
 
 module.exports = router
