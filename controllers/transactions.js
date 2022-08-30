af260b82c0eee7c116b1950c066221491c391d94
iTzAymen <59091390+iTzAymen@users.noreply.github.com>
2022-08-30 11:19:36 +0000

    Update transactions.js

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,7 +3,10 @@ const Transaction = require('../models/Transaction')
 const getAllData = async (req, res) => {
     console.log('getting data')
     try {
+        const d = new Date()
         const transaction = await Transaction.find({})
+        const t = new Date() - d
+        console.log(`query successful after ${t} ms`)
         res.status(200).json({success: true, data: transaction})
     } catch (error) {
         console.log(error)
