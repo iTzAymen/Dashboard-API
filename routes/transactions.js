34a600dbab6b7d79a6345520cad042c23121b904
Aymen <aymeno190@gmail.com>
2022-09-30 14:00:48 +0100

    Fixed searchByID

--- routes/transactions.js
+++ routes/transactions.js
@@ -10,7 +10,7 @@ const {
   searchByID,
 } = require("../controllers/transactions");
 
-router.route("/").post(searchByID);
+router.route("/").get(searchByID);
 
 router.route("/page/:page").get(getOnePage);
 
